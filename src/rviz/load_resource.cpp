02b7b673189a88c1554f7ba5e8203c4c0c61365e
Sean Yen [MSFT] <seanyen@microsoft.com>
2019-02-13 15:15:03 -0800

    rework getpath for Windows. (#22)

--- src/rviz/load_resource.cpp
+++ src/rviz/load_resource.cpp
@@ -42,7 +42,6 @@ namespace rviz
 boost::filesystem::path getPath( QString url )
 {
   boost::filesystem::path path;
-  std::string stdPath = url.toStdString();
 
   if ( url.indexOf("package://", 0, Qt::CaseInsensitive) == 0 )
   {
@@ -55,9 +54,9 @@ boost::filesystem::path getPath( QString url )
   {
     path = url.section('/',2).toStdString();
   }
-  else if (boost::filesystem::exists(stdPath))
+  else if (boost::filesystem::exists(path))
   {
-    path = stdPath;
+    // On Windows, the path would be the file path without any protocol prefix (e.g., "c:\abc\def.rviz").
   }
   else
   {
